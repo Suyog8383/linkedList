5b288202aa5603c6e9bc5b53122c63b755c83d83
Suyog Kalyan Nagawade <92072200+Suyog8383@users.noreply.github.com>
2024-10-24 09:37:10 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,31 @@
+//linkedList
+class Node {
+    constructor(value){
+        this.head = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(value){
+        this.head = new Node(value);
+        this.tail = this.head;
+        this.length = 1
+    }
+    
+    push(value){
+        const newNode=new Node(value);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        this.tail.next=newNode;
+        this.tail = newNode;
+        this.length++
+    }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.push(20);
+myLinkedList.push(30)
+console.log(myLinkedList)
